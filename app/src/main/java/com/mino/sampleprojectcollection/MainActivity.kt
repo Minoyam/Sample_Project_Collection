76cf58b9c4f428fa2136b013588eb2099578b797
Mino <k3000js@naver.com>
2021-06-18 09:17:52 +0900

    init

---
+++ app/src/main/java/com/mino/sampleprojectcollection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mino.sampleprojectcollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
