026bdcce71daeb4f2c848ded18cbfcf9dfe22b3e
Mino <k3000js@naver.com>
2021-06-18 10:48:23 +0900

    init

--- app/src/main/java/com/mino/sampleprojectcollection/MainActivity.kt
+++ app/src/main/java/com/mino/sampleprojectcollection/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.mino.sampleprojectcollection
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText = findViewById<EditText>(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val height = heightEditText.text.toString().toInt()
+            val weight = weightEditText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+            startActivity(intent)
+        }
     }
 }
