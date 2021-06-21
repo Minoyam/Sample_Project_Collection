8610fbf1dd0fa1d24c258a36f2a4dc65316ad6c7
Mino <k3000js@naver.com>
2021-06-21 11:20:14 +0900

    init

--- app/src/main/java/com/mino/sampleprojectcollection/MainActivity.kt
+++ app/src/main/java/com/mino/sampleprojectcollection/MainActivity.kt
@@ -1,11 +1,142 @@
 package com.mino.sampleprojectcollection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton: Button by lazy {
+        findViewById(R.id.clearButton)
+    }
+    private val addButton: Button by lazy {
+        findViewById(R.id.addButton)
+    }
+    private val runButton: Button by lazy {
+        findViewById(R.id.runButton)
+    }
+
+    private val numberPicker: NumberPicker by lazy {
+        findViewById(R.id.numberPicker)
+    }
+
+    private val numberTextViewList: List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById(R.id.textView1),
+            findViewById(R.id.textView2),
+            findViewById(R.id.textView3),
+            findViewById(R.id.textView4),
+            findViewById(R.id.textView5),
+            findViewById(R.id.textView6)
+        )
+    }
+    private var didRun = false
+
+    private val pickNumberSet = hashSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.apply {
+            minValue = 1
+            maxValue = 45
+        }
+
+        initRunButton()
+        initAddButton()
+        initClearButton()
+    }
+
+    private fun initRunButton() {
+        runButton.setOnClickListener {
+            val list = getRandomNumber()
+
+            didRun = true
+
+            list.forEachIndexed { index, number ->
+                numberTextViewList[index].apply {
+                    isVisible = true
+                    text = number.toString()
+                    background = setNumberBackground(number)
+                }
+            }
+        }
+
+    }
+
+    private fun initAddButton() {
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.size >= 5) {
+                Toast.makeText(this, "번호는 5개까지만 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.contains(numberPicker.value)) {
+                Toast.makeText(this, "이미 선택한 번호입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            numberTextViewList[pickNumberSet.size].apply {
+                isVisible = true
+                text = numberPicker.value.toString()
+
+                background = setNumberBackground(numberPicker.value)
+            }
+
+            pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+        }
+    }
+
+    private fun setNumberBackground(number: Int) =
+        when (number) {
+            in 1..10 -> {
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.circle_yello)
+            }
+            in 11..20 -> {
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.circle_blue)
+            }
+            in 21..30 -> {
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.circle_red)
+            }
+            in 31..40 -> {
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.circle_gray)
+            }
+            else -> {
+                ContextCompat.getDrawable(this@MainActivity, R.drawable.circle_green)
+            }
+        }
+
+    private fun getRandomNumber(): List<Int> {
+        val numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if (pickNumberSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
+            }
+        numberList.shuffle()
+        return (pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)).sorted()
     }
 }
